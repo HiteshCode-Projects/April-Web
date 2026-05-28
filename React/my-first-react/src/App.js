d78a94c994efee92ae10bcf35ccd3054d248ba55
HiteshCode-Projects <hiteshtokasiya@gmail.com>
2026-05-28 20:10:47 +0530

    react

---
+++ React/my-first-react/src/App.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Btn from "./Button";
+
+
+function App(){
+
+
+return(
+  //HTML Code-
+<div>
+  
+<h2>Hello Guys</h2>
+<button>Login</button>
+<Btn  />
+<Btn  />
+
+{/* Every Tag Should must be closed */}
+<label>username</label>
+<input    />
+<Btn  />
+<Btn  />
+
+<Btn  />
+<Btn  />
+
+
+</div>
+
+)  
+}
+
+export default App;
+
